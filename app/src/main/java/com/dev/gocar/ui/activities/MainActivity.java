d79a7e1859b7911d8c045c31c82b552397f0ca3c
Andres Paez <andres.paez@innso.co>
2020-04-08 18:09:55 -0500

    [NO_TASK] | Project | Update gradle

--- app/src/main/java/com/dev/gocar/ui/activities/MainActivity.java
+++ app/src/main/java/com/dev/gocar/ui/activities/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
